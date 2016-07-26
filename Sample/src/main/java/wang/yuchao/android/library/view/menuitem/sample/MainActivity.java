8ab69f8204559bcf4ca251c1a4a51a86b77c405b
yuchao-wang <1154786190@qq.com>
2016-07-26 15:52:24 +0800

    包名更改

--- Sample/src/main/java/wang/yuchao/android/library/view/menuitem/sample/MainActivity.java
+++ Sample/src/main/java/wang/yuchao/android/library/view/menuitem/sample/MainActivity.java
@@ -3,8 +3,6 @@ package wang.yuchao.android.library.view.menuitem.sample;
 import android.app.Activity;
 import android.os.Bundle;
 
-import wang.yuchao.android.library.view.sample.menuitem.R;
-
 public class MainActivity extends Activity {
 
     @Override
